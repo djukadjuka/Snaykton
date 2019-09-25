eec39e28be53144c68106502cbec91166bbe305f
Djuka1 <milanns@live.com>
2019-09-25 22:30:33 +0200

    Update main
    Updated main with testing scenarios, updated .gitignore

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+# -- My imports
 import sys
 import PyQt5.QtWidgets as wigs
+import Components.HighScoreEntryWidget as HighScoreEntryWidget
 
-# -- My imports
-import Components.MainWindow as MainWindow
+from Components.HighScorePanel import HighScorePanel
+from Components.ScrollableDataPanel import ScrollableDataPanel
 
 if __name__ == '__main__':
+
     app = wigs.QApplication(sys.argv)
 
-    main_window = MainWindow.MainWindow()
+    # main_window = MainWindow.MainWindow()
+
+    hs_panel = HighScorePanel()
+    hs_panel.show()
+
+    # sdp = ScrollableDataPanel()
 
     app.exec_()
