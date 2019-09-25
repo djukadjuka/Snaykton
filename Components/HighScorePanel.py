fa377c7fd2c8b492b1275718baefb4e07f89f541
Djuka1 <milanns@live.com>
2019-09-25 22:27:09 +0200

    Create High Score Panel
    Created entire panel that presents high scores. This panel contains a sort of 'heading', a scrollable panel with high scores, and a back button. The back button funcionalities are not yet implemented, and should only close the window that contains the panel.

--- Components/HighScorePanel.py
+++ Components/HighScorePanel.py
@@ -1,6 +1,7 @@
 import sys
 import PyQt5.QtWidgets as wigs
 import PyQt5.QtGui as qtgui
+import PyQt5.QtCore as qtcore
 import Controls.HighScoreControls as HighScoreControls
 from Components.ScrollableDataPanel import ScrollableDataPanel
 
@@ -10,8 +11,8 @@ class HighScorePanel(wigs.QWidget):
     def __init__(self, *args, **kwargs):
         super(HighScorePanel, self).__init__(*args, **kwargs)
 
-        self.window_width = 420
-        self.window_height = 640
+        self.min_width = 320
+        self.min_height = 360
 
         # -- Setup methods
         self.__housekeeping()
@@ -19,11 +20,9 @@ class HighScorePanel(wigs.QWidget):
         # -- Setup layouts / components / ..
         self.init_components()
 
-        self.show()
-
     def __housekeeping(self):
-        self.setFixedHeight(self.window_height)
-        self.setFixedWidth(self.window_width)
+        self.setMinimumHeight(self.min_height)
+        self.setMinimumWidth(self.min_width)
         self.header_text = '$ High Scores $'
 
     def init_components(self):
@@ -31,6 +30,7 @@ class HighScorePanel(wigs.QWidget):
         header_label = wigs.QLabel(self)
         header_label.setText(self.header_text)
         header_label_font = qtgui.QFont('Courier', 16, qtgui.QFont.Bold)
+        header_label.setAlignment(qtcore.Qt.AlignHCenter)
         header_label.setFont(header_label_font)
         header_label.setStyleSheet('border: 2px dashed black')
 
@@ -38,22 +38,29 @@ class HighScorePanel(wigs.QWidget):
         scrollable_data_panel = ScrollableDataPanel()
 
         # -- Configure Back Button
+        back_button = wigs.QPushButton()
+        back_button.setText('Back')
+        back_button.clicked.connect(self.__back_button_clicked)
 
         # -- Configure Layout for components
         vbox_layout = wigs.QVBoxLayout()
-        vbox_layout.addStretch(1)
+        # vbox_layout.addStretch(1)
+        vbox_layout.setSpacing(20)
         vbox_layout.addWidget(header_label)
         vbox_layout.addWidget(scrollable_data_panel)
-
-        vbox_layout.addStretch(1)
+        vbox_layout.addWidget(back_button)
+        # vbox_layout.addStretch(1)
 
         # -- After every vertical widg is set, add
         # add the vertical layout to the horizontal layout to put everything in the center
         hbox_layout = wigs.QHBoxLayout()
-        hbox_layout.addStretch(1)
         hbox_layout.addLayout(vbox_layout)
-        hbox_layout.addStretch(1)
+        hbox_layout.setContentsMargins(20, 10, 20, 10)
+
         self.setLayout(hbox_layout)
+
+    def __back_button_clicked(self):
+        print('Back button clicked!')
 
     def __move_window_to_center(self):
         center_point = wigs.QDesktopWidget().availableGeometry().center()
